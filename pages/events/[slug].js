46e10d7b58d566ee19f9e6dd947b4dff81a7fb4f
Romain Grandjean <romain.grandjean@outlook.com>
2021-10-13 09:44:54 +0200

    add delete function implemented

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,13 +1,31 @@
 import { FaPencilAlt, FaTimes } from 'react-icons/fa';
 import Link from 'next/link';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+// Components
 import Layout from '@/components/Layout';
 import { API_URL } from '@/config/index';
 import styles from '@/styles/Event.module.css';
 
 export default function EventPage({ evt }) {
-	const deleteEvent = (e) => {
-		console.log(e);
+	const router = useRouter();
+
+	const deleteEvent = async (e) => {
+		if (confirm('Are you sure ?')) {
+			const res = await fetch(`${API_URL}/events/${evt.id}`, {
+				method: 'DELETE',
+			});
+			const data = await res.json();
+
+			if (!res.ok) {
+				toast.error(data.message);
+			} else {
+				router.push('/events');
+			}
+		}
 	};
 
 	return (
@@ -28,6 +46,7 @@ export default function EventPage({ evt }) {
 					{evt.time}
 				</span>
 				<h1>{evt.name}</h1>
+				<ToastContainer />
 				{evt.image && (
 					<div className={styles.image}>
 						<Image
